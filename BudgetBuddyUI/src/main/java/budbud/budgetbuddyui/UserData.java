f0ce848220ac8d9e39df3035005cd2a3ec4f0292
Damorse <damris2905@gmail.com>
2025-03-19 01:38:30 +0800

    Update UserData.java

--- BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/UserData.java
+++ BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/UserData.java
@@ -1,23 +1,16 @@
 package budbud.budgetbuddyui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserData {
-    private static UserData instance;
-    private String username;
-    private String email;
-    private String phone;
-    private String birthdate;
-    private String password;
+    private final String username;
+    private final String email;
+    private final String phone;
+    private final String birthdate;
+    private final String password;
 
-    private UserData() { }
-
-    public static UserData getInstance() {
-        if (instance == null) {
-            instance = new UserData();
-        }
-        return instance;
-    }
-
-    public void setUserData(String username, String email, String phone, String birthdate, String password) {
+    public UserData(String username, String email, String phone, String birthdate, String password) {
         this.username = username;
         this.email = email;
         this.phone = phone;
@@ -25,19 +18,32 @@ public class UserData {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public String getBirthdate() {
-        return birthdate;
-    }
-    public String getPassword(){
-        return password;
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+    public String getBirthdate() { return birthdate; }
+    public String getPassword() { return password; }
+
+    public static class UserManager {
+        private static final List<UserData> users = new ArrayList<>();
+
+        //adds new user
+        public static void addUser(UserData user) {
+            users.add(user);
+        }
+
+        //checks if the user already has an account
+        public static boolean isUserExists(String username, String email) {
+            for (UserData user : users) {
+                if (user.getUsername().equals(username) || user.getEmail().equals(email)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static List<UserData> getUsers() {
+            return users;
+        }
     }
 }
