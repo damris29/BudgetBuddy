6390444d3e4bc92e9ea2b83cc04dbc1e4944e718
Damorse <damris2905@gmail.com>
2025-03-23 23:24:01 +0800

    Update UserData.java

--- BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/UserData.java
+++ BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/UserData.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserData {
-    private final String username;
-    private final String email;
-    private final String phone;
-    private final String birthdate;
-    private final String password;
+    private String username;
+    private String email;
+    private String phone;
+    private String birthdate;
+    private String password;
 
     public UserData(String username, String email, String phone, String birthdate, String password) {
         this.username = username;
@@ -18,13 +18,21 @@ public class UserData {
         this.password = password;
     }
 
+    //Getter
     public String getUsername() { return username; }
     public String getEmail() { return email; }
     public String getPhone() { return phone; }
     public String getBirthdate() { return birthdate; }
     public String getPassword() { return password; }
 
+    //Setter
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public void setBirthdate(String birthdate) { this.birthdate = birthdate; }
+
     public static class UserManager {
+        private static UserData loggedInUser;  // Store the logged-in user
         private static final List<UserData> users = new ArrayList<>();
 
         //adds new user
@@ -44,6 +52,12 @@ public class UserData {
 
         public static List<UserData> getUsers() {
             return users;
+        }
+        public static void setLoggedInUser(UserData user) {
+            loggedInUser = user;
+        }
+        public static UserData getLoggedInUser() {
+            return loggedInUser;
         }
     }
 }
