3f011e53225c0ada8338410aa281498ab347f304
Damorse <damris2905@gmail.com>
2025-03-24 03:02:48 +0800

    Update ViewBudgetPage.java

--- BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/ViewBudgetPage.java
+++ BudgetBuddyUI/src/main/java/budbud/budgetbuddyui/ViewBudgetPage.java
@@ -6,16 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Optional;
 
 public class ViewBudgetPage {
     @FXML
@@ -23,53 +17,42 @@ public class ViewBudgetPage {
     @FXML private Button btnSet;
     @FXML private Button btnViewBudget;
     @FXML private Button btnSettings;
-    @FXML private ImageView logoutImage;
-
-    Image imageView = new Image(Objects.requireNonNull(getClass().getResourceAsStream("src.main.images/Logout-BBwUI.png")));
+    @FXML private Button btnGoals;
+    @FXML private Button btnTips;
 
     @FXML
     public void initialize() {
         addHoverEffect(btnHome);
         addHoverEffect(btnSet);
         addHoverEffect(btnSettings);
-
-        logoutImage.setOnMouseClicked(e -> handleImageLogout());
-    }
-
-    @FXML
-    private void handleImageLogout() {
-        handleLogout(new ActionEvent(logoutImage, null));
+        addHoverEffect(btnGoals);
+        addHoverEffect(btnTips);
     }
 
     private void addHoverEffect(Button button) {
-        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: #26284a;")); //changes color when hover mouse
-        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #323667;")); //back to original color
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: #5b5b5b;")); //changes color when hover mouse
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #6d6d6d;")); //back to original color
     }
 
     @FXML
-    private void handleLogout(ActionEvent event){
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Logout!");
-        alert.setHeaderText("You are about to logout");
-        alert.setContentText("Are you sure you want to logout?");
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.isPresent() && result.get() == ButtonType.OK) {
-            loadScene(event, "LoginPage.fxml", "Budget Buddy - Main Page");
-        }
-    }
-
-    @FXML
-    public void toHome(ActionEvent event){
-        loadScene(event, "HomePage.fxml", "Budget Buddy - Home Page");
+    public void toHome (ActionEvent event){
+        loadScene(event, "HomePage.fxml", "Budget Buddy - Set Budget");
     }
     @FXML
-    public void toBudgetSet(ActionEvent event){
-        loadScene(event, "SetBudget.fxml", "Budget Buddy - Set Budget");
+    public void toSetBudget(ActionEvent event){
+        loadScene(event, "SetBudget.fxml", "Budget Buddy - Budget Viewer");
     }
     @FXML
     public void toSetting(ActionEvent event){
         loadScene(event, "SetProfile.fxml", "Budget Buddy - Settings");
+    }
+    @FXML
+    public void toGoals(ActionEvent event){
+        loadScene(event, "GoalsPage.fxml", "Budget Buddy - Saving Goals");
+    }
+    @FXML
+    public void toTips(ActionEvent event){
+        loadScene(event, "FinTipsPage.fxml", "Budget Buddy - Financial Tips");
     }
 
     private void loadScene(ActionEvent e, String fxmlFile, String title) {
